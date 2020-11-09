97dec19ed3bf4f09d4287b2b8066540b1f5a6bf5
Alone2 <alain.sinzig@gmail.com>
2020-11-09 20:35:47 +0100

    test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { SystemNotification, SystemNotificationListener } from 'capacitor-notificationlistener';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
 
+  ngOnInit() {
+    console.log('starting...');
+    const sn = new SystemNotificationListener();
+    sn.requestPermission();
+    sn.startListening();
+
+    sn.addListener('notificationReceivedEvent', (info: SystemNotification) => {
+      console.log('notificationReceivedEvent', info);
+    });
+    sn.addListener('notificationRemovedEvent', (info: SystemNotification) => {
+      console.log('notificationRemovedEvent', info);
+    });
+  }
 }
